6977e00cba70e59337a79eb09bb3bbf5844c5ff2
ErikCodeGit <erik.mueller0912@gmail.com>
2022-11-23 08:09:32 +0100

    Add save and load logic

--- lib/display.rb
+++ lib/display.rb
@@ -12,16 +12,31 @@ module Display
 
   def ask_which_save
     if Dir.exist?('saves')
-      puts Dir.glob('saves/*').(map { |file| file[(file.index('/') + 1)...(file.index('.'))] })
-      Dir.home
-      gets.chomp
+      file_names = Dir.glob('saves/*').map { |file| file[(file.index('/') + 1)...(file.index('.'))] }
+      puts(file_names.map { |fname| "- #{fname}" })
+      puts 'Which save do you want to load?'
+      fname = ''
+      loop do
+        fname = gets.chomp
+        break if file_names.include?(fname)
+
+        puts 'Please enter a valid filename!'
+      end
+      fname
     else
       puts 'You did not save any games yet!'
+      Dir.mkdir('saves')
+      false
     end
   end
 
   def display_hint(hint)
     formatted_hint = hint.chars.join(' ')
     puts formatted_hint
+  end
+
+  def ask_save_name
+    puts "Enter the name for your save: "
+    gets.chomp
   end
 end
--- lib/game.rb
+++ lib/game.rb
@@ -13,26 +13,39 @@ class Game
     when 'new'
       new_game
     end
+    # save_game(ask_save_name)
   end
 
   private
 
   def new_game
+    puts 'Starting new Game!'
     p @word = choose_random_word
-    p @hint = @word.gsub(/./, '_')
+    @hint = @word.gsub(/./, '_')
+    @guesses = 12
     display_hint(@hint)
   end
 
   def save_game(fname)
+    file = File.new("saves/#{fname}.yml", 'w')
     @info = { word: @word, hint: @hint, guesses: @guesses }
-    File.open(fname) { |file| file.write(@info.to_yaml) }
+    file.write(@info.to_yaml)
   end
 
   def load_save(fname)
-    content = YAML.safe_load(File.read(`"#{fname}".yml`))
-    @word = content[:word]
-    @hint = content[:hint]
-    @guesses = content[:guesses]
+    if fname
+      content = YAML.load(File.read("saves/#{fname}.yml"))
+      begin
+        @word = content[:word]
+        @hint = content[:hint]
+        @guesses = content[:guesses]
+        puts 'Loaded file successfully!'
+      rescue StandardError
+        puts "Couldn't load from file!"
+      end
+    else
+      new_game
+    end
   end
 
   def choose_random_word
