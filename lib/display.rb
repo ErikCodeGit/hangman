8725032065150fe0c5bebdc574d0cdff164c3063
ErikCodeGit <erik.mueller0912@gmail.com>
2022-11-23 14:37:41 +0100

    Clean up console output

--- lib/display.rb
+++ lib/display.rb
@@ -1,18 +1,25 @@
 # frozen_string_literal: true
 
 module Display
+  def display_hr
+    puts Array.new(50, '-').join
+  end
+
   def ask_new_or_load
     puts '- Enter 1 to start a new game'
     puts '- Enter 2 to load a saved game'
     respone = gets.chomp
+    display_hr
     return 'load' if respone == '2'
 
     'new' if respone == '1'
   end
 
   def ask_which_save
+    display_hr
     if Dir.exist?('saves')
       file_names = Dir.glob('saves/*').map { |file| file[(file.index('/') + 1)...(file.index('.'))] }
+      puts 'Your saved games:'
       puts(file_names.map { |fname| "- #{fname}" })
       puts 'Which save do you want to load?'
       fname = ''
@@ -26,17 +33,55 @@ module Display
     else
       puts 'You did not save any games yet!'
       Dir.mkdir('saves')
-      false
+      display_hr
+      return false
     end
+    display_hr
   end
 
   def display_hint(hint)
     formatted_hint = hint.chars.join(' ')
     puts formatted_hint
+    puts
   end
 
   def ask_save_name
-    puts "Enter the name for your save: "
+    puts 'Enter the name for your save: '
     gets.chomp
+  end
+
+  def ask_letter
+    print 'Enter your guess: '
+    response = ''
+    loop do
+      response = gets.chomp
+      return response if response == '!save'
+      break if response.length == 1 && response.match?(/[a-zA-Z]/)
+
+      puts 'Invalid guess, only enter single letters:'
+    end
+    display_hr
+    response.downcase
+  end
+
+  def display_guesses(guesses)
+    puts "You have #{guesses} guesses left!"
+  end
+
+  def display_game_start
+    puts 'Starting new Game!'
+    display_hr
+  end
+
+  def display_load_success
+    puts 'Loaded file successfully!'
+  end
+
+  def display_load_error
+    puts "Couldn't load from file!"
+  end
+
+  def display_save_success
+    puts 'Saved file successfully!'
   end
 end
